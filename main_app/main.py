1e1d6731a073487d77befad49ddf41ba2970f1f7
Dylan Bennett <me@mboffin.com>
2025-02-07 11:22:21 -0800

    Main library listing grid working

--- main_app/main.py
+++ main_app/main.py
@@ -1,19 +1,77 @@
-import zmq, sys
-from PySide6 import QtCore as qtc
+import zmq, sys, random
 from PySide6 import QtWidgets as qtw
+from PySide6 import QtCore as qtc
 from PySide6 import QtGui as qtg
 
 from ui.main_window_ui import Ui_w_MainWindow
 
 class MainWindow(qtw.QMainWindow, Ui_w_MainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent=parent)
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
+        self.populate_tips()
 
+        self.btn_begin.clicked.connect(self.begin)
+        self.pnl_pages.currentChanged.connect(self.page_changed)
+
+        self.show()
+
+    # TODO: Replace with microservice
+    def populate_tips(self):
+        tips = [
+            "This is a tip.",
+            "This is another tip.",
+            "This is a great tip.",
+            "This is an amazing tip.",
+            "Everything in this app is amazing."
+        ]
+        while len(tips) > 2:
+            tips.pop(random.randint(0, len(tips)-1))
+        tip_text = ""
+        for tip in tips:
+            tip_text += f"• {tip}\n\n"
+        self.lbl_tips.setText(tip_text)
+
+    def begin(self):
+        self.pnl_pages.setCurrentIndex(1)
+
+    def page_changed(self, index):
+        if index == 1:
+            owned_games = [1, 2, 3, 4, 5, 6, 7, 8]
+            r = 0
+            c = 0
+            for game in owned_games:
+                game_listing = GameListing()
+                self.grid_games.addWidget(game_listing, r, c)
+                c += 1
+                if c >= 4:
+                    c = 0
+                    r += 1
+
+class GameListing(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = qtw.QVBoxLayout()
+        self.image = qtw.QLabel()
+        self.image.setPixmap(qtg.QPixmap("./ui/images/placeholder.png"))
+        self.image.setMinimumSize(120, 180)
+        self.image.setMaximumSize(120, 180)
+        self.image.setScaledContents(True)
+        self.image.setSizePolicy(qtw.QSizePolicy.Policy.Minimum, qtw.QSizePolicy.Policy.Minimum)
+
+        self.lbl_name = qtw.QLabel("Game Name")
+        self.lbl_genre = qtw.QLabel("Genre")
+        self.lbl_year = qtw.QLabel("Year")
+
+        layout.addWidget(self.image)
+        layout.addWidget(self.lbl_name)
+        layout.addWidget(self.lbl_genre)
+        layout.addWidget(self.lbl_year)
+
+        self.setLayout(layout)
 
 if __name__ == "__main__":
     app = qtw.QApplication()
     window = MainWindow()
-    window.show()
 
     sys.exit(app.exec())
--- main_app/ui/main_window_ui.py
+++ main_app/ui/main_window_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'main_window.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.1
+## Created by: Qt User Interface Compiler version 6.8.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QHBoxLayout,
-    QLabel, QMainWindow, QPushButton, QSizePolicy,
-    QSpacerItem, QStackedWidget, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QGroupBox,
+    QHBoxLayout, QLabel, QMainWindow, QPushButton,
+    QScrollArea, QSizePolicy, QSpacerItem, QStackedWidget,
+    QVBoxLayout, QWidget)
 
 class Ui_w_MainWindow(object):
     def setupUi(self, w_MainWindow):
@@ -29,13 +30,18 @@ class Ui_w_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(w_MainWindow.sizePolicy().hasHeightForWidth())
         w_MainWindow.setSizePolicy(sizePolicy)
+        w_MainWindow.setMinimumSize(QSize(640, 480))
+        w_MainWindow.setMaximumSize(QSize(640, 480))
         w_MainWindow.setBaseSize(QSize(640, 480))
+        w_MainWindow.setWindowTitle(u"Simple Game Library")
         self.centralwidget = QWidget(w_MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.stackedWidget = QStackedWidget(self.centralwidget)
-        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.pnl_pages = QStackedWidget(self.centralwidget)
+        self.pnl_pages.setObjectName(u"pnl_pages")
         self.intro_page = QWidget()
         self.intro_page.setObjectName(u"intro_page")
         self.verticalLayout = QVBoxLayout(self.intro_page)
@@ -46,6 +52,7 @@ class Ui_w_MainWindow(object):
         font.setPointSize(36)
         font.setBold(True)
         self.lbl_title.setFont(font)
+        self.lbl_title.setText(u"Simple Game Library")
         self.lbl_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout.addWidget(self.lbl_title)
@@ -55,6 +62,8 @@ class Ui_w_MainWindow(object):
         font1 = QFont()
         font1.setPointSize(20)
         self.lbl_subtitle.setFont(font1)
+        self.lbl_subtitle.setText(u"Keeping track of your games,\n"
+"so you don't have to.")
         self.lbl_subtitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout.addWidget(self.lbl_subtitle)
@@ -70,6 +79,7 @@ class Ui_w_MainWindow(object):
         font2.setPointSize(18)
         font2.setBold(True)
         self.groupBox.setFont(font2)
+        self.groupBox.setTitle(u"Did you know...")
         self.groupBox.setFlat(False)
         self.groupBox.setCheckable(False)
         self.verticalLayout_2 = QVBoxLayout(self.groupBox)
@@ -77,6 +87,7 @@ class Ui_w_MainWindow(object):
         self.verticalLayout_2.setContentsMargins(32, -1, -1, 16)
         self.lbl_tips = QLabel(self.groupBox)
         self.lbl_tips.setObjectName(u"lbl_tips")
+        self.lbl_tips.setText(u"tips text")
 
         self.verticalLayout_2.addWidget(self.lbl_tips)
 
@@ -107,6 +118,7 @@ class Ui_w_MainWindow(object):
         font3.setBold(True)
         self.btn_begin.setFont(font3)
         self.btn_begin.setAutoFillBackground(False)
+        self.btn_begin.setText(u"Begin")
         self.btn_begin.setFlat(False)
 
         self.horizontalLayout.addWidget(self.btn_begin)
@@ -118,18 +130,123 @@ class Ui_w_MainWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.stackedWidget.addWidget(self.intro_page)
+        self.pnl_pages.addWidget(self.intro_page)
         self.library_page = QWidget()
         self.library_page.setObjectName(u"library_page")
-        self.stackedWidget.addWidget(self.library_page)
+        self.verticalLayout_3 = QVBoxLayout(self.library_page)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.widget = QWidget(self.library_page)
+        self.widget.setObjectName(u"widget")
+        self.horizontalLayout_2 = QHBoxLayout(self.widget)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.btnHome = QPushButton(self.widget)
+        self.btnHome.setObjectName(u"btnHome")
+        sizePolicy.setHeightForWidth(self.btnHome.sizePolicy().hasHeightForWidth())
+        self.btnHome.setSizePolicy(sizePolicy)
+        self.btnHome.setMinimumSize(QSize(72, 72))
+        self.btnHome.setMaximumSize(QSize(72, 72))
+        font4 = QFont()
+        font4.setPointSize(14)
+        font4.setBold(True)
+        self.btnHome.setFont(font4)
+        self.btnHome.setText(u"\ud83c\udfe0\n"
+"Home")
 
-        self.gridLayout.addWidget(self.stackedWidget, 0, 0, 1, 1)
+        self.horizontalLayout_2.addWidget(self.btnHome)
+
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+        font5 = QFont()
+        font5.setPointSize(26)
+        font5.setBold(True)
+        self.label.setFont(font5)
+        self.label.setText(u"Game Library")
+        self.label.setScaledContents(False)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.widget_4 = QWidget(self.widget)
+        self.widget_4.setObjectName(u"widget_4")
+        sizePolicy.setHeightForWidth(self.widget_4.sizePolicy().hasHeightForWidth())
+        self.widget_4.setSizePolicy(sizePolicy)
+        self.widget_4.setMinimumSize(QSize(72, 72))
+        self.widget_4.setMaximumSize(QSize(72, 72))
+
+        self.horizontalLayout_2.addWidget(self.widget_4)
+
+
+        self.verticalLayout_3.addWidget(self.widget)
+
+        self.widget_2 = QWidget(self.library_page)
+        self.widget_2.setObjectName(u"widget_2")
+        self.horizontalLayout_3 = QHBoxLayout(self.widget_2)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.btn_add_game = QPushButton(self.widget_2)
+        self.btn_add_game.setObjectName(u"btn_add_game")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.btn_add_game.sizePolicy().hasHeightForWidth())
+        self.btn_add_game.setSizePolicy(sizePolicy3)
+        self.btn_add_game.setFont(font4)
+        self.btn_add_game.setStyleSheet(u"QPushButton {\n"
+"	padding: 10px;\n"
+"}")
+        self.btn_add_game.setText(u"\u2795 Add Game")
+
+        self.horizontalLayout_3.addWidget(self.btn_add_game)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_3)
+
+        self.dd_sort_by = QComboBox(self.widget_2)
+        self.dd_sort_by.addItem(u"Name")
+        self.dd_sort_by.addItem(u"Genre")
+        self.dd_sort_by.addItem(u"Year")
+        self.dd_sort_by.setObjectName(u"dd_sort_by")
+        self.dd_sort_by.setFont(font4)
+        self.dd_sort_by.setStyleSheet(u"QComboBox {\n"
+"	padding: 10px;\n"
+"}")
+
+        self.horizontalLayout_3.addWidget(self.dd_sort_by)
+
+
+        self.verticalLayout_3.addWidget(self.widget_2)
+
+        self.widget_3 = QWidget(self.library_page)
+        self.widget_3.setObjectName(u"widget_3")
+        self.verticalLayout_4 = QVBoxLayout(self.widget_3)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.sa_games = QScrollArea(self.widget_3)
+        self.sa_games.setObjectName(u"sa_games")
+        self.sa_games.setWidgetResizable(True)
+        self.w_game_grid = QWidget()
+        self.w_game_grid.setObjectName(u"w_game_grid")
+        self.w_game_grid.setGeometry(QRect(0, 0, 620, 326))
+        self.grid_games = QGridLayout(self.w_game_grid)
+        self.grid_games.setObjectName(u"grid_games")
+        self.sa_games.setWidget(self.w_game_grid)
+
+        self.verticalLayout_4.addWidget(self.sa_games)
+
+
+        self.verticalLayout_3.addWidget(self.widget_3)
+
+        self.pnl_pages.addWidget(self.library_page)
+
+        self.gridLayout.addWidget(self.pnl_pages, 0, 0, 1, 1)
 
         w_MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(w_MainWindow)
 
-        self.stackedWidget.setCurrentIndex(0)
+        self.pnl_pages.setCurrentIndex(0)
         self.btn_begin.setDefault(True)
 
 
@@ -137,12 +254,7 @@ class Ui_w_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, w_MainWindow):
-        w_MainWindow.setWindowTitle(QCoreApplication.translate("w_MainWindow", u"Simple Game Library", None))
-        self.lbl_title.setText(QCoreApplication.translate("w_MainWindow", u"Simple Game Library", None))
-        self.lbl_subtitle.setText(QCoreApplication.translate("w_MainWindow", u"Keeping track of your games\n"
-"so you don't have to.", None))
-        self.groupBox.setTitle(QCoreApplication.translate("w_MainWindow", u"Did you know...", None))
-        self.lbl_tips.setText(QCoreApplication.translate("w_MainWindow", u"tips text", None))
-        self.btn_begin.setText(QCoreApplication.translate("w_MainWindow", u"Begin", None))
+
+        pass
     # retranslateUi
 
